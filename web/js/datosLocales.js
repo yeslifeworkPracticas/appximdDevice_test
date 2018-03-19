e74cba90944ff180a83b7ef49cbfcf2869679b71
user <user@YLW-05>
2018-03-19 11:12:12 +0100

    Edición version SO Windows.

--- web/js/datosLocales.js
+++ web/js/datosLocales.js
@@ -1,45 +1,70 @@
+function datosLocales() {
+    var d = new Date();
+    var dia = d.getDate();
+    var mes = d.getMonth();
+    var año = d.getFullYear();
+    mes += 1;
+    if (mes < 10)
+        mes = "0" + mes;
 
-function datosLocales(){
-var d = new Date();
-var dia = d.getDate();
-var mes = d.getMonth();
-var año = d.getFullYear();
-mes += 1;
-if (mes < 10)mes = "0" + mes;
+    var fecha = dia + "/" + mes + "/" + año;
 
-var fecha = dia + "/" + mes + "/" + año;
+    var OSName = "Desconocido";
+    if (navigator.appVersion.indexOf("Win") !== -1) {
+        OSName = "Windows";
+    } else if (navigator.appVersion.indexOf("Mac") !== -1) {
+        OSName = "MacOS";
+    } else if (navigator.appVersion.indexOf("Linux") !== -1) {
+        OSName = "Linux";
+    } else if (navigator.appVersion.indexOf("Android") !== -1) {
+        OSName = "Android";
+    } else {
+        OSName = "Otros";
+    }
 
-var OSName = "Desconocido";
-if (navigator.appVersion.indexOf("Win") !== -1) {
-    OSName = "Windows";
-} else if (navigator.appVersion.indexOf("Mac") !== -1) {
-    OSName = "MacOS";
-} else if (navigator.appVersion.indexOf("Linux") !== -1) {
-    OSName = "Linux";
-} else if (navigator.appVersion.indexOf("Android") !== -1) {
-    OSName = "Android";
-} else {
-    OSName = "Otros";
-}
-var navegador = "";
-if (navigator.userAgent.search("Chrome") >= 0) {
-    navegador = "Chrome";
-}
-if (navigator.userAgent.search("Firefox") >= 0) {
-    navegador = "Firefox";
-}
-if (navigator.userAgent.search("MSIE") >= 0) {
-    navegador = "Internet Explorer";
-}
-if (navigator.userAgent.search("Opera") >= 0) {
-    navegador = "Opera";
-}
-if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-    navegador = "Safari";
-}
-document.forms['reg']['fecha'].value = "Fecha     "+fecha; 
-document.forms['reg']['so'].value = "Sistema Operativo   "+OSName ;
-document.forms['reg']['nav'].value ="Navegador     "+navegador;
+    var navegador = "";
+    if (navigator.userAgent.search("Chrome") >= 0) {
+        navegador = "Chrome";
+    }
+    if (navigator.userAgent.search("Firefox") >= 0) {
+        navegador = "Firefox";
+    }
+    if (navigator.userAgent.search("MSIE") >= 0) {
+        navegador = "Internet Explorer";
+    }
+    if (navigator.userAgent.search("Opera") >= 0) {
+        navegador = "Opera";
+    }
+    if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+        navegador = "Safari";
+    }
+
+
+    var OSName = "Otro";
+    if (window.navigator.userAgent.indexOf("Windows NT 10.0") != -1)
+        OSName = "Windows 10";
+    if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1)
+        OSName = "Windows 8";
+    if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1)
+        OSName = "Windows 7";
+    if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1)
+        OSName = "Windows Vista";
+    if (window.navigator.userAgent.indexOf("Windows NT 5.1") != -1)
+        OSName = "Windows XP";
+    if (window.navigator.userAgent.indexOf("Windows NT 5.0") != -1)
+        OSName = "Windows 2000";
+    if (window.navigator.userAgent.indexOf("Mac") != -1)
+        OSName = "Mac/iOS";
+    if (window.navigator.userAgent.indexOf("X11") != -1)
+        OSName = "UNIX";
+    if (window.navigator.userAgent.indexOf("Linux") != -1)
+        OSName = "Linux";
+    if (window.navigator.userAgent.indexOf("Android") != -1)
+        OSName = "Android";
+
+    document.forms['reg']['fecha'].value = "Fecha:     " + fecha;
+    document.forms['reg']['so'].value = "Sistema Operativo:   " + OSName;
+    document.forms['reg']['nav'].value = "Navegador:     " + navegador;
 }
 
 
--- web/js/local_db.js
+++ web/js/local_db.js
@@ -25,14 +25,14 @@ function LoadDB() {
         tx.executeSql("SELECT * FROM sqlite_master WHERE name ='device' and type='table'", [], function (tx, result) {
 
             if (result.rows.length > 0) {
-                alert('la bbdd existe');
+                alert('Dispositivo registrado');
                 mostrarDatos();
             } else {
-                alert('la bbdd no existe');
+                alert('Dispositivo no registrado. Redireccionando al registro');
                 document.location.replace("registro.html");
             }
         }, function (tx, error) {
-            //alert('la bbdd no existe');
+            alert('Error');
             return;
         });
     });
@@ -42,7 +42,7 @@ function crearTabla() {
     db.transaction(function (tx) {
         tx.executeSql("create table device(fecha TEXT, so TEXT, nav TEXT, alias TEXT UNIQUE)", [], function (result) {});
     });
-    alert('tabla creada');
+    //alert('tabla creada');
 }
 
 function crearRegistro() {
@@ -50,7 +50,7 @@ function crearRegistro() {
         tx.executeSql("insert into device values(?,?,?,?) ", [document.querySelector("#fecha").value, document.querySelector("#so").value, document.querySelector("#nav").value, document.querySelector("#alias").value]);
         document.location.replace("index.html");
     });
-    alert('Registro insertado');
+    //alert('Registro insertado');
 }
 
 function mostrarDatos() {
@@ -60,18 +60,18 @@ function mostrarDatos() {
             /*var schemanode = document.getElementById('schema');
              schemanode.innerHTML = "";*/
 
-           // for (var i = 0; i < result.rows.length; ++i) {
-                var row = result.rows.item(0);
-                //alert("alias: "+row["alias"]);
-                document.forms['login']['alias_device'].value = row["alias"]; 
-                
+            // for (var i = 0; i < result.rows.length; ++i) {
+            var row = result.rows.item(0);
+            //alert("alias: "+row["alias"]);
+            document.forms['login']['alias_device'].value = row["alias"];
 
-                /*var notediv = document.createElement('div');
-                 notediv.innerHTML = 'fecha: ' + row['fecha'] + ', so: ' + row['so'] + ', nav ' + row['navegador'] + ', alias ' + row['alias'];
-                 schemanode.appendChild(notediv);*/
-            }
+
+            /*var notediv = document.createElement('div');
+             notediv.innerHTML = 'fecha: ' + row['fecha'] + ', so: ' + row['so'] + ', nav ' + row['navegador'] + ', alias ' + row['alias'];
+             schemanode.appendChild(notediv);*/
+        }
         /*}*/, function (tx, error) {
-            alert('Failed to retrieve notes from database - ' + error.message);
+            //alert('Failed to retrieve notes from database - ' + error.message);
             return;
         });
     });
@@ -82,6 +82,6 @@ function borrarRegistro() {
     }, function () {
     }, function () {
     });
-    alert('Registro borrado');
+    //alert('Registro borrado');
 }
             
